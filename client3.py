3a10732254a3814f3c4d2fcb4114ac112dd046d4
yashasviagarwal28 <yashasviagarwal28@gmail.com>
2024-07-20 15:41:05 +0530

    task 1

--- client3.py
+++ client3.py
@@ -31,7 +31,6 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
@@ -41,7 +40,6 @@ def getDataPoint(quote):
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
     if (price_b == 0):
         return
     return price_a/price_b
@@ -54,7 +52,7 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+
         prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
